a70b74f4b2493d4e847805ad1f5caf57821e6037
Mark Pitchless <markpitchless@gmail.com>
2013-12-02 19:35:29 +0000

    Publish clouds as recognised objects.

--- src/cluster_segmentor_node.cpp
+++ src/cluster_segmentor_node.cpp
@@ -13,18 +13,29 @@
  **/
 
 #include <ros/ros.h>
+#include "sr_point_cloud/cluster_segmentor.h"
 
 #include <dynamic_reconfigure/server.h>
 #include "sr_point_cloud/ClusterSegmentorConfig.h"
 
-#include "sr_point_cloud/cluster_segmentor.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl_ros/point_cloud.h" // Allow use of PCL cloud types for pubs and subs
+
+#include <sensor_msgs/PointCloud2.h>
+#include <object_recognition_msgs/RecognizedObject.h>
+#include <object_recognition_msgs/RecognizedObjectArray.h>
+
 
 namespace sr_point_cloud {
+
+using namespace std;
+using namespace object_recognition_msgs;
 
 class ClusterSegmentorNode {
 
 public:
     typedef pcl::PointXYZ PointType;
+    typedef pcl::PointCloud<PointType> Cloud;
 
     /**
      * Construct a new ClusterSegmentorNode, setting up it's publishers,
@@ -32,19 +43,8 @@ public:
      */
     ClusterSegmentorNode() : nh_("~")
     {
-        // Setup ROS topics and services
-    }
-
-    /**
-     * Handles incoming dynamic reconfigure requests, updating the classes
-     * members from the config.
-     */
-    void config_cb_(sr_point_cloud::ClusterSegmentorConfig &config, uint32_t level)
-    {
-      //cluster_segmentor_.setUseConvexHull(config.use_convex_hull);
-      cluster_segmentor_.setClusterTolerance(config.cluster_tolerance);
-      cluster_segmentor_.setMinClusterSize(config.min_cluster_size);
-      cluster_segmentor_.setMaxClusterSize(config.max_cluster_size);
+        input_cloud_sub_ = nh_.subscribe("input/points", 1, &ClusterSegmentorNode::cloud_cb_, this);
+        output_objects_pub_ = nh_.advertise<RecognizedObjectArray>("output/clusters", 1);
     }
 
     /**
@@ -57,9 +57,40 @@ public:
 
 protected:
     ros::NodeHandle nh_;
+    Cloud::ConstPtr input_cloud_;
     ClusterSegmentor<PointType> cluster_segmentor_;
-
     dynamic_reconfigure::Server<sr_point_cloud::ClusterSegmentorConfig> config_server_;
+    ros::Subscriber input_cloud_sub_;
+    ros::Publisher output_objects_pub_;
+
+    void config_cb_(sr_point_cloud::ClusterSegmentorConfig &config, uint32_t level)
+    {
+      //cluster_segmentor_.setUseConvexHull(config.use_convex_hull);
+      cluster_segmentor_.setClusterTolerance(config.cluster_tolerance);
+      cluster_segmentor_.setMinClusterSize(config.min_cluster_size);
+      cluster_segmentor_.setMaxClusterSize(config.max_cluster_size);
+    }
+
+    void cloud_cb_(const Cloud::ConstPtr& cloud)
+    {
+      vector<Cloud::Ptr> clusters;
+      input_cloud_ = cloud;
+      cluster_segmentor_.setInputCloud(input_cloud_);
+      ROS_INFO("Segmenting cloud...");
+      cluster_segmentor_.extract(clusters);
+      ROS_INFO("... found %i clusters", (int)clusters.size());
+      RecognizedObjectArray out;
+      for (vector<Cloud::Ptr>::const_iterator it = clusters.begin(); it != clusters.end(); ++it)
+      {
+        Cloud::Ptr cluster_cloud = *it;
+        RecognizedObject obj;
+        sensor_msgs::PointCloud2 pc2;
+        pcl::toROSMsg(*cluster_cloud, pc2);
+        obj.point_clouds.push_back(pc2);
+        out.objects.push_back(obj);
+      }
+      output_objects_pub_.publish(out);
+    }
 };
 
 } //sr_point_cloud
