fe7821c081610670426299c783210a6b45c5f02d
Mark Pitchless <markpitchless@gmail.com>
2013-12-09 18:23:14 +0000

    Add action server.
    * Fix bug where config was not being set.
    * Fix NaN bug by adding a filter to remove NaN from cloud.
    * Add action server.

--- src/cluster_segmentor_node.cpp
+++ src/cluster_segmentor_node.cpp
@@ -20,11 +20,13 @@
 
 #include "pcl_conversions/pcl_conversions.h"
 #include "pcl_ros/point_cloud.h" // Allow use of PCL cloud types for pubs and subs
+#include <pcl/filters/filter.h>
 
 #include <sensor_msgs/PointCloud2.h>
 #include <object_recognition_msgs/RecognizedObject.h>
 #include <object_recognition_msgs/RecognizedObjectArray.h>
-
+#include <object_recognition_msgs/ObjectRecognitionAction.h>
+#include <actionlib/server/simple_action_server.h>
 
 namespace sr_point_cloud {
 
@@ -36,15 +38,21 @@ class ClusterSegmentorNode {
 public:
     typedef pcl::PointXYZ PointType;
     typedef pcl::PointCloud<PointType> Cloud;
+    typedef actionlib::SimpleActionServer<ObjectRecognitionAction> RecognitionServer;
 
     /**
      * Construct a new ClusterSegmentorNode, setting up it's publishers,
      * subscribers, etc.
      */
-    ClusterSegmentorNode() : nh_("~")
+    ClusterSegmentorNode()
+      : nh_("~")
+      , remove_nan_(true)
+      , recognize_objects_as_(nh_, "recognize_objects", boost::bind(&ClusterSegmentorNode::recognize_objects_execute_cb_, this, _1), false)
     {
+        config_server_.setCallback( boost::bind(&ClusterSegmentorNode::config_cb_, this, _1, _2) );
         input_cloud_sub_ = nh_.subscribe("input/points", 1, &ClusterSegmentorNode::cloud_cb_, this);
         output_objects_pub_ = nh_.advertise<RecognizedObjectArray>("output/clusters", 1);
+        recognize_objects_as_.start();
     }
 
     /**
@@ -58,10 +66,12 @@ public:
 protected:
     ros::NodeHandle nh_;
     Cloud::ConstPtr input_cloud_;
+    bool remove_nan_;
     ClusterSegmentor<PointType> cluster_segmentor_;
     dynamic_reconfigure::Server<sr_point_cloud::ClusterSegmentorConfig> config_server_;
     ros::Subscriber input_cloud_sub_;
     ros::Publisher output_objects_pub_;
+    RecognitionServer recognize_objects_as_;
 
     void config_cb_(sr_point_cloud::ClusterSegmentorConfig &config, uint32_t level)
     {
@@ -73,13 +83,41 @@ protected:
 
     void cloud_cb_(const Cloud::ConstPtr& cloud)
     {
+      if (remove_nan_)
+      {
+        // Convex hull does not like a cloud with NaN in (which kinect give us).
+        std::vector<int> indices;
+        Cloud::Ptr clean_cloud(new Cloud);
+        pcl::removeNaNFromPointCloud(*cloud, *clean_cloud, indices);
+        input_cloud_ = clean_cloud;
+        //ROS_INFO_STREAM("Segmenting cloud: " << *cloud << "\n" << *input_cloud_);
+      }
+      else
+        input_cloud_ = cloud;
+
+      RecognizedObjectArray out = extract_();
+      output_objects_pub_.publish(out);
+    }
+
+    void recognize_objects_execute_cb_(const RecognitionServer::GoalConstPtr& goal)
+    {
+      RecognizedObjectArray objs = extract_();
+      ObjectRecognitionResult res;
+      res.recognized_objects = objs;
+      recognize_objects_as_.setSucceeded(res);
+    }
+
+    RecognizedObjectArray extract_()
+    {
+      RecognizedObjectArray out;
+      if (!input_cloud_->points.size() > 0)
+        return out;
+
       vector<Cloud::Ptr> clusters;
-      input_cloud_ = cloud;
       cluster_segmentor_.setInputCloud(input_cloud_);
       ROS_INFO("Segmenting cloud...");
       cluster_segmentor_.extract(clusters);
       ROS_INFO("... found %i clusters", (int)clusters.size());
-      RecognizedObjectArray out;
       for (vector<Cloud::Ptr>::const_iterator it = clusters.begin(); it != clusters.end(); ++it)
       {
         Cloud::Ptr cluster_cloud = *it;
@@ -89,7 +127,7 @@ protected:
         obj.point_clouds.push_back(pc2);
         out.objects.push_back(obj);
       }
-      output_objects_pub_.publish(out);
+      return out;
     }
 };
 
