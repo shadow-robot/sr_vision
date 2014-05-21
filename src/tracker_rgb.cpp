4b638ca5d6fed616479164780e7e0275d79a6c08
Yi Li <yi@shadowrobot.com>
2014-05-21 12:03:52 +0100

    Add PMD Nano tracker.

--- src/tracker_rgb.cpp
+++ src/tracker_rgb.cpp
@@ -1,10 +1,10 @@
-#include "sr_point_cloud/tracker.hpp"
+#include "sr_point_cloud/tracker_rgb.hpp"
 
 int
 main (int argc, char** argv)
 {
   ros::init (argc, argv, "sr_point_cloud");
-  sr_point_cloud::Tracker<pcl::PointXYZ> node;
+  sr_point_cloud::TrackerRGB node;
   node.run();
   return 0;
 }
