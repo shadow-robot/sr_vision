de8962b540d67d2d53a9155de8dd46d0394a1859
Yi Li <yi@shadowrobot.com>
2014-05-22 10:22:39 +0100

    Change node names.

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -3,7 +3,7 @@
 int
 main (int argc, char** argv)
 {
-  ros::init (argc, argv, "sr_point_cloud");
+  ros::init (argc, argv, "point_cloud_tracker");
   sr_point_cloud::Tracker<pcl::PointXYZ> node;
   node.run();
   return 0;
--- src/tracker_rgb.cpp
+++ src/tracker_rgb.cpp
@@ -3,7 +3,7 @@
 int
 main (int argc, char** argv)
 {
-  ros::init (argc, argv, "sr_point_cloud");
+  ros::init (argc, argv, "point_cloud_tracker_rgb");
   sr_point_cloud::TrackerRGB node;
   node.run();
   return 0;
