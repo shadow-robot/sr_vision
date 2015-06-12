fd00315c850a7a2637ed152e323ef510d38b2dbf
mewine <mewiiine@gmail.com>
2015-06-12 11:10:06 +0100

    Visualization package renamed v2

--- sr_gui_servoing/setup.py
+++ sr_gui_servoing/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['sr_displaying'],
+    packages=['sr_gui_servoing'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
