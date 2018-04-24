2d8947c38a4fbb67719b0e0bb7e962c28a9f074d
mikramarc <michal@shadowrobot.com>
2018-04-24 11:34:40 +0000

    added spawn and move objecy

---
+++ sr_vision_mocks/src/sr_vision_mocks/move_object.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
+
+# Used together with spawn_object.py node
+# Example use:
+# rosrun sr_vision_mocks move_object.py -o duplo_2x4x1_0 -p 0.6 0.701 0.7633 3.14159265359 0.0 1.57079632679
+
+import rospy
+import argparse
+import tf
+from sr_msgs_common.srv import MoveObject
+from geometry_msgs.msg import Pose
+
+
+def request_pick_and_place_sim(object_name, place_pose):
+    pose = Pose()
+    pose.position.x = place_pose[0]
+    pose.position.y = place_pose[1]
+    pose.position.z = place_pose[2]
+
+    quat = tf.transformations.quaternion_from_euler(place_pose[3], place_pose[4], place_pose[5])
+    pose.orientation.x = quat[0]
+    pose.orientation.y = quat[1]
+    pose.orientation.z = quat[2]
+    pose.orientation.w = quat[3]
+
+    rospy.loginfo("Picking up object")
+    rospy.wait_for_service('move_sim_object')
+    try:
+        move_sim_object = rospy.ServiceProxy('/move_sim_object', MoveObject)
+        response = move_sim_object(object_name, pose)
+        rospy.loginfo("Object placed in new position")
+        return response
+    except rospy.ServiceException, e:
+        rospy.logerr("Service call failed")
+
+if __name__ == '__main__':
+    rospy.init_node('mating_map_mock_client', anonymous=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--object_name', type=str, help='Name of the duplo block frame')
+    parser.add_argument('-p', '--place_position', nargs=6, help='Pose to place object to')
+    args = parser.parse_args()
+
+    place_pose = [float(val) for val in args.place_position]
+    chosen_object = args.object_name
+
+    rospy.loginfo("Chose {} to be picked".format(chosen_object))
+    rospy.loginfo("Moving duplo to position: {}".format(place_pose))
+
+    pick_and_place_successful = request_pick_and_place_sim(chosen_object, place_pose)
+
+    if pick_and_place_successful:
+        rospy.loginfo("Successfully moved object".format(place_pose))
---
+++ sr_vision_mocks/src/sr_vision_mocks/spawn_object.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Shadow Robot Company Ltd - All Rights Reserved. Proprietary and Confidential.
+# Unauthorized copying of the content in this file, via any medium is strictly prohibited.
+
+# example use:
+# rosrun sr_vision_mocks spawn_object.py -p "duplo_2x4x1" 0.7 0.6 0.763 0 0 1.57 -p "utl5_small" 0.7 0.7 0.763 0 0 0
+
+import rospy
+import argparse
+import tf
+import tf2_ros
+import rospkg
+import re
+from gazebo_msgs.srv import DeleteModel
+from geometry_msgs.msg import TransformStamped, Pose
+from gazebo_ros.gazebo_interface import spawn_sdf_model_client
+from sr_msgs_common.srv import MoveObject
+
+
+class DebugFramePublisher:
+    def __init__(self, sim=False):
+        self.broadcaster = tf2_ros.StaticTransformBroadcaster()
+        self.moving_object_service = rospy.Service('/move_sim_object', MoveObject, self.move_object_CB)
+        self.tf_object_names = []
+        self.tf_object_poses = []
+        self.tf_object_transforms = []
+        self.sim = sim
+
+    def process_args(self, poses_list): 
+        for pose in poses_list:
+            idx = 0
+            tf_name = pose.pop(0) + "_"
+            for object_name in self.tf_object_names:
+                if re.match(tf_name, object_name):
+                    idx += 1
+            self.tf_object_names.append(tf_name + str(idx))
+            self.tf_object_poses.append([float(val) for val in pose])
+
+        for name, pose in zip(self.tf_object_names, self.tf_object_poses):
+            self.tf_object_transforms.append(self.get_transform(name, pose[0:3], pose[3:6]))
+
+    def broadcast_frames(self):
+        self.broadcaster.sendTransform(self.tf_object_transforms)
+
+    def change_duplo_position(self, moved_object_name, new_pose):
+        for i, object_name in enumerate(self.tf_object_names):
+            if moved_object_name == object_name:
+                self.tf_object_poses[i] = new_pose
+                self.tf_object_transforms[i] = self.get_transform(moved_object_name,
+                                                                  self.tf_object_poses[i][0:3],
+                                                                  self.tf_object_poses[i][3:6])
+                break
+
+    def move_object_CB(self, req):
+        rospy.loginfo("Moving object {} to position {}".format(req.object_id, req.place_pose))
+        place_orientation = tf.transformations.euler_from_quaternion([req.place_pose.orientation.x,
+                                                                      req.place_pose.orientation.y,
+                                                                      req.place_pose.orientation.z,
+                                                                      req.place_pose.orientation.w])
+
+        self.change_duplo_position(req.object_id, [req.place_pose.position.x,
+                                                   req.place_pose.position.y,
+                                                   req.place_pose.position.z,
+                                                   place_orientation[0],
+                                                   place_orientation[1],
+                                                   place_orientation[2]])
+        self.broadcast_frames()
+        return True
+
+    @staticmethod
+    def get_transform(name, position, rotation):
+        static_transform_stamped = TransformStamped()
+        static_transform_stamped.header.stamp = rospy.Time.now()
+
+        static_transform_stamped.header.frame_id = "world"
+        static_transform_stamped.child_frame_id = name
+
+        static_transform_stamped.transform.translation.x = position[0]
+        static_transform_stamped.transform.translation.y = position[1]
+        static_transform_stamped.transform.translation.z = position[2]
+
+        quat = tf.transformations.quaternion_from_euler(rotation[0], rotation[1], rotation[2])
+        static_transform_stamped.transform.rotation.x = quat[0]
+        static_transform_stamped.transform.rotation.y = quat[1]
+        static_transform_stamped.transform.rotation.z = quat[2]
+        static_transform_stamped.transform.rotation.w = quat[3]
+        return static_transform_stamped
+
+    @staticmethod
+    def get_pose(position, rotation):
+        pose = Pose()
+
+        pose.position.x = position[0]
+        pose.position.y = position[1]
+        pose.position.z = position[2]
+
+        quat = tf.transformations.quaternion_from_euler(rotation[0], rotation[1], rotation[2])
+        pose.orientation.x = quat[0]
+        pose.orientation.y = quat[1]
+        pose.orientation.z = quat[2]
+        pose.orientation.w = quat[3]
+        return pose
+
+if __name__ == '__main__':
+    rospy.init_node('my_static_tf2_broadcaster')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--sim', action='store_true', help='Use this flag if running in simulation')
+    parser.add_argument('-p', '--pose', action='append', nargs=7, help='Block position, use as three ' +
+                                                                       'floats separated by space sign only')
+    args = parser.parse_args()
+
+    frame_pub = DebugFramePublisher(args.sim)
+    frame_pub.process_args(args.pose)
+    frame_pub.broadcast_frames()
+
+    rospy.spin()
